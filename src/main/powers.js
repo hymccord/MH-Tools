dbadaec38c7589711e7763c57bb00464e502d230
Hank McCord <hymccord@gmail.com>
2024-08-13 10:24:56 -0400

    Send child id with messages

--- src/main/powers.js
+++ src/main/powers.js
@@ -125,6 +125,7 @@ window.onload = function() {
       const charm = row.find("td:nth-child(4)").text();
 
       child.sendMessageToParent({
+        id: child.tabId,
         msg: "arm",
         components: {
           trinket: charm,
